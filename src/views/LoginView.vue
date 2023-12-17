713e7784b270cb84cc9147d6db7352861c989349
tony tolver <andrademaresantonio00@gmail.com>
2023-12-16 23:54:11 -0600

    ADD Formulario de login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,5 +3,41 @@
 </script>
 
 <template>
-   <h1 class="text-center text-h3 font-weight-bold">Iniciar Sesion</h1>
+   <v-card
+      flat
+      max-width="600"
+      class="mx-auto my-10"
+   >
+      <v-card-title
+         class="text-h4 font-weight-bold"
+      >
+         Iniciar Sesion
+      </v-card-title>
+      <v-card-subtitle>
+         Inicia sesion con tu cuenta
+      </v-card-subtitle>
+      <v-form class="mt-5">
+         <v-text-field
+            type="email"
+            label="Email"
+            bg-color="blue-grey-lighten-5"
+            required
+         >
+         </v-text-field>
+         <v-text-field
+            type="password"
+            label="Password"
+            bg-color="blue-grey-lighten-5"
+            required
+         >
+         </v-text-field>
+         <v-btn
+            block   
+            color="pink-accent-3"
+         >
+            Iniciar Sesion
+         </v-btn>
+      </v-form>
+   </v-card>
+
 </template>
