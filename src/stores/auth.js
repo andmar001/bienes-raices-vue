08cb73eb0169360a9dbfe0c476c7ba74d71d53e5
tony tolver <andrademaresantonio00@gmail.com>
2023-12-21 15:00:38 -0600

    ADD Identificar al usuario autenticado

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,21 +1,29 @@
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { defineStore } from 'pinia'
 import { useFirebaseAuth } from 'vuefire';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
 export const useAuthStore = defineStore('auth', () => {
 
    const auth = useFirebaseAuth()
-   const authUser = ref({})
+   const authUser = ref(null)
 
    const errorMsg = ref('')
-   
    const errorCodes = {
          'auth/invalid-email': 'Email inválido',
          'auth/user-disabled': 'Usuario desabilitado',
          'auth/user-not-found': 'Usuario no encontrado',
          'auth/wrong-password': 'Contraseña incorrecta',
       }
+
+   onMounted(() => {
+      onAuthStateChanged(auth, (user) => {
+         if (user) {
+            authUser.value = user
+         }
+      })
+   })
+
    const login = ({ email,password }) => {
       signInWithEmailAndPassword(auth, email, password)
          .then((userCredential) => {
@@ -26,7 +34,7 @@ export const useAuthStore = defineStore('auth', () => {
          })
          .catch( error => {
             console.log(error)
-            errorMsg.value = errorCodes[error.code] || 'Error desconocido';
+            errorMsg.value = errorCodes[error.code];
          });
    }
 
