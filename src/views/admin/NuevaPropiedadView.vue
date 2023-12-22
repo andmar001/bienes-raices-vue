6e18f3117dcc5ea208960498b30b6a2700f925cf
tony tolver <andrademaresantonio00@gmail.com>
2023-12-21 19:59:16 -0600

    ADD Mapear state con campos de formulario de propiedades

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -10,6 +10,14 @@
       }
    })
 
+   const titulo = useField('titulo')
+   const imagen = useField('imagen')
+   const precio = useField('precio')
+   const habitaciones = useField('habitaciones')
+   const wc = useField('wc')
+   const estacionamiento = useField('estacionamiento')
+   const descripcion = useField('descripcion')
+
    const items = [1, 2, 3, 4, 5]
 
    const submit = handleSubmit(( values ) => {
@@ -36,12 +44,16 @@
          <v-text-field
             class="mb-5"
             label="Título Propiedad"
+            v-model="titulo.value.value"
+            :error-messages="titulo.errorMessage.value"
          />            
          <v-file-input
             accept="image/jpeg"
             label="Fotografía"
             prepend-icon="mdi-camera"
             class="mb-5"
+            v-model="imagen.value.value"
+            :error-messages="imagen.errorMessage.value"
          />
          <v-text-field
             class="mb-5"
@@ -54,6 +66,8 @@
                   label="Habitaciones"
                   class="mb-5"
                   :items="items"
+                  v-model="habitaciones.value.value"
+                  :error-messages="habitaciones.errorMessage.value"
                />
             </v-col>
             
@@ -62,6 +76,8 @@
                   label="WC"
                   class="mb-5"
                   :items="items"
+                  v-model="wc.value.value"
+                  :error-messages="wc.errorMessage.value"
                />
             </v-col>
             
@@ -70,11 +86,19 @@
                   label="Lugares Estacionamiento"
                   class="mb-5"
                   :items="items"
+                  v-model="estacionamiento.value.value"
+                  :error-messages="estacionamiento.errorMessage.value"
                />
             </v-col>
          </v-row>
 
-         <v-textarea class="mb-5" label="Descripcion"></v-textarea>
+         <v-textarea 
+            class="mb-5" 
+            label="Descripcion"
+            v-model="descripcion.value.value"
+            :error-messages="descripcion.errorMessage.value"   
+         > </v-textarea>
+
          <v-checkbox label="Alberca"/>
 
          <v-btn
