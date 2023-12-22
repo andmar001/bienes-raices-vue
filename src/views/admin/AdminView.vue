2398cea6b0861ae1b2dd05c7a2001e44e836742e
tony tolver <andrademaresantonio00@gmail.com>
2023-12-21 19:23:06 -0600

    ADD Formulario de nuevas propiedades

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -4,5 +4,12 @@
 
 <template>
    <h2 class="text-center text-h3 my-5 font-weight-bold">Admin</h2>
+   <v-btn
+      color="primary"
+      variant="flat"
+      :to="{ name: 'nueva-propiedad' }"
+   >
+      Nueva Propiedad
+   </v-btn>
 </template>
 
